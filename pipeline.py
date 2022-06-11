569329e34ef8508f4bcd478c1bd2e59e1fef6220
James Douglass <jdouglass@stanford.edu>
2022-06-10 17:04:24 -0700

    Adding URLs to the soil grids files.

--- pipeline.py
+++ pipeline.py
@@ -7,19 +7,30 @@ import taskgraph
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger('ISRIC-AWC')
+URL_BASE = 'https://files.isric.org/soilgrids/former/2017-03-10/data'
+ISRIC_2017_RASTERS = {
+    '0cm':   f'{URL_BASE}/WWP_M_sl1_250m_ll.tif',
+    '5cm':   f'{URL_BASE}/WWP_M_sl2_250m_ll.tif',
+    '15cm':  f'{URL_BASE}/WWP_M_sl3_250m_ll.tif',
+    '30cm':  f'{URL_BASE}/WWP_M_sl4_250m_ll.tif',
+    '60cm':  f'{URL_BASE}/WWP_M_sl5_250m_ll.tif',
+    '100cm': f'{URL_BASE}/WWP_M_sl6_250m_ll.tif',
+    '200cm': f'{URL_BASE}/WWP_M_sl7_250m_ll.tif',
+}
 
 
 def fetch_raster(source_raster_path, dest_raster_path, checksum):
     pass
 
+
 def calculate_awc(
-        depth_0cm_path,
-        depth_5cm_path,
-        depth_15cm_path,
-        depth_30cm_path,
-        depth_60cm_path,
-        depth_100cm_path,
-        depth_200cm_path,
+        soil_depth_0cm_path,
+        soil_depth_5cm_path,
+        soil_depth_15cm_path,
+        soil_depth_30cm_path,
+        soil_depth_60cm_path,
+        soil_depth_100cm_path,
+        soil_depth_200cm_path,
         target_awc_path):
     # determine nodata values for each layer.
 
@@ -29,6 +40,8 @@ def calculate_awc(
     # TODO: make this a COG
     # TODO: be sure to compress the output raster
     # TODO: build overviews as well before upload.
+    # TODO: build in some warnings if the values are outside of the expected
+    # range of 0-100 (or 0-1 if we've already divided by 100).
     pygeoprocessing.raster_calculator()
 
 
